9eff35028ccc689e0c37fc6be14ee548caa7d900
schmittmax91@gmail.com <schmittmax91@gmail.com@0fb3cef8-0ce9-bc03-309d-426d669aeca9>
2013-11-13 23:41:33 +0000

    oberfläche

--- trunk/src/de/image/model/settings/Settings.java
+++ trunk/src/de/image/model/settings/Settings.java
@@ -1,22 +1,58 @@
 package de.image.model.settings;
 
+import java.io.File;
 import java.util.ResourceBundle;
 
 public final class Settings {
 
 	private static Settings singleton;
 
+	private ResourceBundle resBundle;
+
 	// ##### window settings #####
 	private String windowTitle;
 	private int windowWidth;
 	private int windowHeight;
+	private String windowIcon;
+
+	// ##### image settings #####
+	private String imageDir;
+	private String imagePlaceholder;
+
+	private String chooserTitle;
 
 	private Settings() {
-		ResourceBundle resBundle = ResourceBundle.getBundle("settings");
+		resBundle = ResourceBundle.getBundle("settings");
 
-		setWindowTitle(resBundle.getString("window_title"));
-		setWindowWidth(Integer.parseInt(resBundle.getString("window_width")));
-		setWindowHeight(Integer.parseInt(resBundle.getString("window_height")));
+		setWindowTitle(getProperty("window_title", "Kicker BallTracking"));
+		setWindowWidth(getIntProperty("window_width", 1200));
+		setWindowHeight(getIntProperty("window_height", 900));
+
+		setImageDir(getProperty("image_dir", "E:\\Praktikum Master\\Bilder"));
+		setImagePlaceholder(getProperty("image_placeholder",
+				"images/img_placeholder"));
+		setChooserTitle(getProperty("folderchooser_title", "Choose Folder"));
+		setWindowIcon(getProperty("window_icon", "images/icon.png"));
+	}
+
+	private String getProperty(String key, String defaultValue) {
+		String value = "";
+		try {
+			value = resBundle.getString(key);
+		} catch (Exception e) {
+			return defaultValue;
+		}
+		return value;
+	}
+
+	private int getIntProperty(String key, int defaultValue) {
+		int value = 0;
+		try {
+			value = Integer.parseInt(resBundle.getString(key));
+		} catch (Exception e) {
+			return defaultValue;
+		}
+		return value;
 	}
 
 	public static Settings getInstance() {
@@ -48,6 +84,39 @@ public final class Settings {
 
 	public void setWindowHeight(int windowHeight) {
 		this.windowHeight = windowHeight;
+	}
+
+	public String getImageDir() {
+		return imageDir;
+	}
+
+	public void setImageDir(String imageDir) {
+		imageDir = imageDir.replace("/", File.separator);
+		this.imageDir = imageDir;
+	}
+
+	public String getImagePlaceholder() {
+		return imagePlaceholder;
+	}
+
+	public void setImagePlaceholder(String imagePlaceholder) {
+		this.imagePlaceholder = imagePlaceholder;
+	}
+
+	public String getChooserTitle() {
+		return chooserTitle;
+	}
+
+	public void setChooserTitle(String chooserTitle) {
+		this.chooserTitle = chooserTitle;
+	}
+
+	public String getWindowIcon() {
+		return windowIcon;
+	}
+
+	public void setWindowIcon(String windowIcon) {
+		this.windowIcon = windowIcon;
 	}
 
 }
