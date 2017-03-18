ab4923c6150821c2d8b5b5acab63305dec592997
Maximilian Schmitt <schmittmax91@gmail.com>
2017-03-18 01:23:54 +0100

    Erweiterung Statistik (Anfang Vergleich)

--- kicker-balltracking/src/main/java/de/kicker/tracking/view/StatisticsDialog.java
+++ kicker-balltracking/src/main/java/de/kicker/tracking/view/StatisticsDialog.java
@@ -26,6 +26,7 @@ import org.apache.log4j.Logger;
 import java.text.DecimalFormat;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Set;
 
 class StatisticsDialog extends Stage {
 
@@ -122,7 +123,7 @@ class StatisticsDialog extends Stage {
 		tmpVBox.setPadding(new Insets(0));
 		absoluteAutoContainer.getChildren().add(tmpVBox);
 
-		addAbsoluteStats(tmpVBox, (AbstractBallTracking) automaticBallTracking, trackingFactory.getInitialIndex(), maxIndex);
+		addAbsoluteStats(tmpVBox, (AbstractBallTracking) automaticBallTracking, maxIndex);
 
 		tmpHBox = FXUtil.getHBox(-1, -1, 5);
 		tmpHBox.setPadding(new Insets(15));
@@ -134,23 +135,68 @@ class StatisticsDialog extends Stage {
 		tmpVBox.setPadding(new Insets(0));
 		absoluteManualContainer.getChildren().add(tmpVBox);
 
-		addAbsoluteStats(tmpVBox, manualBallTracking, trackingFactory.getInitialIndex(), maxIndex);
+		addAbsoluteStats(tmpVBox, manualBallTracking, maxIndex);
 
-		tmpVBox = FXUtil.getVBox(-1, -1, 0);
-		tmpVBox.setMinHeight(400);
-		tmpVBox.setPadding(new Insets(0));
-		root.getChildren().add(tmpVBox);
+		VBox compareContainer = FXUtil.getVBox(-1, -1, 0);
+		compareContainer.setMinHeight(400);
+		compareContainer.setPadding(new Insets(0));
+		root.getChildren().add(compareContainer);
 
 		tmpHBox = FXUtil.getHBox(-1, -1, 5);
 		tmpHBox.setPadding(new Insets(15));
 		tmpHBox.setAlignment(Pos.CENTER);
 		tmpHBox.getChildren().add(new Label("Compare"));
-		tmpVBox.getChildren().add(tmpHBox);
+		compareContainer.getChildren().add(tmpHBox);
+
+		tmpVBox = FXUtil.getVBox(-1, -1, 0);
+		tmpVBox.setPadding(new Insets(0));
+		compareContainer.getChildren().add(tmpVBox);
 
 		logger.debug("hier kommt die Statistik");
+		GridPane grid = new GridPane();
+		grid.setHgap(15);
+		grid.setVgap(8);
+		grid.setAlignment(Pos.CENTER);
+		ColumnConstraints cc = new ColumnConstraints();
+		cc.setHalignment(HPos.RIGHT);
+		grid.getColumnConstraints().add(cc);
+		cc = new ColumnConstraints();
+		cc.setHalignment(HPos.LEFT);
+		grid.getColumnConstraints().add(cc);
+		tmpVBox.getChildren().add(grid);
+
+		Set<Integer> autoIndizes = automaticBallTracking.getTrackedIndizes();
+		Set<Integer> manualIndizes = manualBallTracking.getTrackedIndizes();
+		Collection<TrackingImage> autoImages = automaticBallTracking.getAllTrackedImages();
+		Collection<TrackingImage> manualImages = manualBallTracking.getAllTrackedImages();
+
+		int max = Math.max(autoIndizes.stream().max(Integer::compareTo).get(), manualIndizes.stream().max(Integer::compareTo).get());
+		int falsePositives = 0;
+		int falseNegatives = 0;
+		int totalCompare = 0;
+		for (int i = trackingFactory.getInitialIndex(); i < max; i++) {
+			TrackingImage autoImg = automaticBallTracking.getTrackingImage(i);
+			TrackingImage manualImg = manualBallTracking.getTrackingImage(i);
+			if (autoImg != null && manualImg != null) {
+				totalCompare++;
+				if (!Objects.equals(autoImg.getPosition(), Position.POSITION_NOT_FOUND) && Objects.equals(manualImg.getPosition(), Position.POSITION_NOT_FOUND)) {
+					falsePositives++;
+				} else if (Objects.equals(autoImg.getPosition(), Position.POSITION_NOT_FOUND) && !Objects.equals(manualImg.getPosition(), Position.POSITION_NOT_FOUND)) {
+					falseNegatives++;
+				}
+			}
+		}
+
+		grid.add(new Label("false positives:"), 0, 0);
+		grid.add(new Label(String.valueOf(falsePositives) + " (" +
+				FORMAT.format((falsePositives / (double) totalCompare) * 100) + "%)"), 1, 0);
+
+		grid.add(new Label("false negatives:"), 0, 1);
+		grid.add(new Label(String.valueOf(falseNegatives) + " (" + FORMAT.format((falseNegatives / (double) totalCompare) *
+				100) + "%)"), 1, 1);
 	}
 
-	private void addAbsoluteStats(VBox container, AbstractBallTracking bt, int initialIndex, int maxIndex) {
+	private void addAbsoluteStats(VBox container, AbstractBallTracking bt, int maxIndex) {
 		GridPane grid = new GridPane();
 		grid.setHgap(15);
 		grid.setVgap(8);
